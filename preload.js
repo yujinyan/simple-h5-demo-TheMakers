e59eca9a16fdf1b67263b6630d6676e3f01d32d6
Jinyan Yu <yujinyan@outlook.com>
2016-02-11 20:16:06 +0800

    fix some browser stuck loading bug

--- preload.js
+++ preload.js
@@ -5,21 +5,27 @@ var loaded = 0;
 /* remove animation on the first section, add it dynamically after preload finishes */
 $('#page-1 img').removeClass('fadeInUp');
 
+/* preload page background image */
 $('.section').not('.preload').each(function() {
     var bg = $(this).css('background-image');
     var original = bg;
     $(this).css('background-image','');
-    bg = bg.replace('url("','').replace('")','');
-    var image  = new Image();
-    image.src = bg;
+    if (bg.match('"')) {
+        bg = bg.replace('url("','').replace('")','');
+    } else {
+        bg = bg.replace('url(','').replace(')','');
+    };
+    var bg_image  = new Image();
+    bg_image.src = bg;
+    bgs.push(bg_image);
     var that = $(this);
-    image.addEventListener('load', function() {
-        afterLoad();
+    bg_image.addEventListener('load', function(e) {
+        afterLoad(e);
         that.css('background-image', original);
     });
-    bgs.push(image);
 });
 
+/* preload images */
 $('img').not('.preload img').each(function() {
     var img = $(this).attr('src');
     var original = img;
@@ -29,13 +35,13 @@ $('img').not('.preload img').each(function() {
     image.src = img;
     images.push(image);
     var that = $(this);
-    image.addEventListener('load', function() {
-        afterLoad();
+    image.addEventListener('load', function(e) {
+        afterLoad(e);
         that.attr('src', original);
     });
 });
 
-function afterLoad() {
+function afterLoad(e) {
     ++loaded;
     var loadPercent = Math.floor(loaded/(bgs.length+images.length)*100) + '%';
     $('#status').text(loadPercent);
@@ -43,8 +49,7 @@ function afterLoad() {
         $('.preload').fadeOut('slow');
         setTimeout(function() {
             $('#page-1 img').addClass('fadeInUp');
-        }, 100);
-        
+        }, 100);    
     }
 }
 
